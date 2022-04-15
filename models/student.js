cc37b065dc6c5c41a7e988047be3d999641fc350
kartikey sood <kartikeysood@rediffmail.com>
2022-04-15 18:35:00 +0530

    w32323

--- models/student.js
+++ models/student.js
@@ -73,8 +73,4 @@ const StudentSchema = new mongoose.Schema({
 });
 
 /////////////////////////////////////////////////////
-<<<<<<< HEAD
 module.exports = mongoose.model("student", StudentSchema);
-=======
-module.exports = mongoose.model("students", StudentSchema);
->>>>>>> 0de238f10d8d8af55e878b11def2443bc5902e64
