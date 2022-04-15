67168e9dc43640789b3c6aea7c72783c0afb68ed
kartikey sood <kartikeysood@rediffmail.com>
2022-04-15 15:38:46 +0530

    agent data base

--- routes/agentRoutes.js
+++ routes/agentRoutes.js
@@ -6,7 +6,7 @@ router.post("/home", agent.create); //create agent profile
 router.post("/update/:agentID"); //update agent profile
 router.post("/getStudent/doc", agent.getStudentDoc); //get student documents
 router.post("/createDoc", agent.agentDocCreate); //create document for agent
-router.put("/upadteDoc", agent.updateAgentDoc); //upadte document for agent
+router.put("/updateDoc", agent.updateAgentDoc); //update document for agent
 router.delete("/deleteDoc", agent.agentDeleteDoc); //delete document for agent
 
 module.exports = router;
