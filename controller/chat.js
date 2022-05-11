673a4c4b771c5637ca772242db9d2f6130f0c077
kartikey sood <kartikeysood@rediffmail.com>
2022-05-11 18:24:20 +0530

    hello

--- controller/chat.js
+++ controller/chat.js
@@ -1,25 +1,29 @@
 const ChatDoc = require("../models/chatDocs");
 
 const addChatDoc = async (req, res) => {
-	const { chatId, docData } = req.body;
-	let data = await ChatDoc.findOne({ chatId });
-	if (!data) {
-		data = new ChatDoc({
-			chatId,
-		});
-	}
-	data.chatDoc.push(docData);
-	data.save();
-	return res.send(data);
+  const { chatId, docData } = req.body;
+  let data = await ChatDoc.findOne({ chatId });
+  if (!data) {
+    data = new ChatDoc({
+      chatId,
+    });
+  }
+  data.chatDoc.push(docData);
+  data.save();
+  return res.send(data);
 };
 
 const getChatDoc = async (req, res) => {
-	const { chatId } = req.body;
-	const data = await ChatDoc.findOne({ chatId });
-	return res.send({ data: data.chatDoc });
+  const { chatId } = req.body;
+  const data = await ChatDoc.findOne({ chatId });
+  if (data) {
+    return res.send({ data: data.chatDoc });
+  } else {
+    return res.send({ data: [] });
+  }
 };
 
 module.exports = {
-	addChatDoc,
-	getChatDoc,
+  addChatDoc,
+  getChatDoc,
 };
