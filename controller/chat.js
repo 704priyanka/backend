115fefad208f627b787081b72b6031918dad4e53
kartikey sood <kartikeysood@rediffmail.com>
2022-05-11 18:27:06 +0530

    made code more error free

--- controller/chat.js
+++ controller/chat.js
@@ -1,25 +1,41 @@
 const ChatDoc = require("../models/chatDocs");
 
 const addChatDoc = async (req, res) => {
-  const { chatId, docData } = req.body;
-  let data = await ChatDoc.findOne({ chatId });
-  if (!data) {
-    data = new ChatDoc({
-      chatId,
-    });
+  try {
+    const { chatId, docData } = req.body;
+
+    if (!chatId) {
+      throw { message: "Chat id required" };
+    }
+
+    let data = await ChatDoc.findOne({ chatId });
+    if (!data) {
+      data = new ChatDoc({
+        chatId,
+      });
+    }
+    data.chatDoc.push(docData);
+    data.save();
+    return res.send(data);
+  } catch (e) {
+    return res.status(500).send(e);
   }
-  data.chatDoc.push(docData);
-  data.save();
-  return res.send(data);
 };
 
 const getChatDoc = async (req, res) => {
-  const { chatId } = req.body;
-  const data = await ChatDoc.findOne({ chatId });
-  if (data) {
-    return res.send({ data: data.chatDoc });
-  } else {
-    return res.send({ data: [] });
+  try {
+    const { chatId } = req.body;
+    if (!chatId) {
+      throw { message: "Chat id required" };
+    }
+    const data = await ChatDoc.findOne({ chatId });
+    if (data) {
+      return res.send({ data: data.chatDoc });
+    } else {
+      return res.send({ data: [] });
+    }
+  } catch (e) {
+    return res.status(500).send(e);
   }
 };
 
